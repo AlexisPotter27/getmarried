eb59f036d38393f5e06198908e9ee0b917f95035
alexis-integrated <obiekwealex@gmail.com>
2023-05-20 19:54:01 +0100

    init

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,13 +3,13 @@ const entitlementID = 'premium';
 
 //TO DO: add your subscription terms and conditions
 const footerText =
-    """ A purchase will be applied to your account upon confirmation and approval. Read our policy: https://getmarriedapp.com/security-and-privacy-policy """;
+""" A purchase will be applied to your account upon confirmation and approval. Read our policy: https://getmarriedapp.com/security-and-privacy-policy """;
 
 //TO DO: add the Apple API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const appleApiKey = 'appl_KEkceXiLeOGCbvHGidLExMHDykU';
+const appleApiKey = 'appl_XWZlhcEkesNwkmyPmcxJNMkJkkX';
 
 //TO DO: add the Google API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const googleApiKey = 'goog_pVGsQqZrcnygSIIOZzGzDpwBFtb';
+const googleApiKey = 'goog_VolvlJikxUaeRLgdQMSokvUSyWZ';
 
 //TO DO: add the Amazon API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
 const amazonApiKey = 'amazon_api_key';
